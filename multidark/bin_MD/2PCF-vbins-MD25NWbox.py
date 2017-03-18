cdc7dad7121cea929af5cd81a778f204b8723370
JohanComparat <johan.comparat@gmail.com>
2017-03-18 22:49:01 +0100

    r 60

--- multidark/bin_MD/2PCF-vbins-MD25NWbox.py
+++ multidark/bin_MD/2PCF-vbins-MD25NWbox.py
@@ -3,6 +3,6 @@ from MultiDark import *
 box = MultiDarkSimulation(Lbox=2500.0 * uu.Mpc, boxDir = "MD_2.5GpcNW")
 
 ll = n.array( glob.glob( "/data2/DATA/eBOSS/Multidark-lightcones/MD_2.5GpcNW/snapshots/out_80*.fits" ) )
-box.compute2PCF_MASS(ll, rmax=140, dr = 0.1, Nmax=2000000, vmin=n.log10(box.Melement)+1, dlogBin=0.1,  name="rmax_140")
+box.compute2PCF_MASS(ll, rmax=60, dr = 0.1, Nmax=2000000, vmin=n.log10(box.Melement)+1, dlogBin=0.1,  name="rmax_60")
 
 
--- multidark/bin_MD/2PCF-vbins-MD40box.py
+++ multidark/bin_MD/2PCF-vbins-MD40box.py
@@ -3,7 +3,7 @@ from MultiDark import *
 box = MultiDarkSimulation(Lbox=4000.0 * uu.Mpc, boxDir = "MD_4Gpc")
 
 ll = n.array( glob.glob( "/data2/DATA/eBOSS/Multidark-lightcones/MD_4Gpc/snapshots/out_128*.fits" ) )
-box.compute2PCF_MASS(ll, rmax=140, dr = 0.1, Nmax=2000000, vmin=n.log10(box.Melement)+1, dlogBin=0.1,  name="rmax_140")
+box.compute2PCF_MASS(ll, rmax=60, dr = 0.1, Nmax=2000000, vmin=n.log10(box.Melement)+1, dlogBin=0.1,  name="rmax_60")
 
 
 
