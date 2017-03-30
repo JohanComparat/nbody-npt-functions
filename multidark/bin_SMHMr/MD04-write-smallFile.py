46c9b1273c7bbf9080c0977bd56f1e7a2bdb2d29
Johan Comparat <comparat@mpe.mpg.de>
2017-03-30 10:35:50 +0200

    SMHMR

--- multidark/bin_SMHMr/MD04-write-smallFile.py
+++ multidark/bin_SMHMr/MD04-write-smallFile.py
@@ -1,12 +1,16 @@
-import MultiDark as md
+from MultiDark import * 
 import os
 import numpy as n
 import glob
 
-snList = n.array(glob.glob(os.path.join(os.environ["MD04"], "snapshots", "out_*.list")))
+snList = n.array(glob.glob(os.path.join(os.environ["MD04"], "snapshots", "hlist_*.list")))
+print snList
+box = MultiDarkSimulation(Lbox=400.0 * uu.Mpc, boxDir = "MD_0.4Gpc", snl =snList   ,zsl = None, zArray = n.arange(0.2,2.4,1e-1), Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
+box.columnDict = box.columnDictHlist
 
-box = md.MultiDarkSimulation(Lbox=400.0 * uu.Mpc, boxDir = "MD_0.4Gpc", snl =snList   ,zsl = None, zArray = n.arange(0.2,2.4,1e-1), Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
+t0=time.time()
 
 for ii in n.arange(len(box.snl)):
-	box.writeSAMcatalog(ii, mmin=1000*box.Melement)
+	box.writeSAMcatalog(ii, mmin=100*box.Melement)
 
+print time.time()-t0
--- multidark/bin_SMHMr/MD10-write-smallFile.py
+++ multidark/bin_SMHMr/MD10-write-smallFile.py
@@ -3,10 +3,14 @@ import sys
 import glob
 import os 
 
-snList= n.array(glob.glob(os.path.join(os.environ["MD10"], "snapshots", "out_*.list")))
-
+snList= n.array(glob.glob(os.path.join(os.environ["MD10"], "snapshots", "hlist_*.list")))
+print snList
 box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc, boxDir = "MD_1Gpc",snl =snList   ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
+box.columnDict = box.columnDictHlist
+
+t0=time.time()
 
 for ii in n.arange(len(box.snl)):
-	box.writeSAMcatalog(ii, mmin=1000*box.Melement)
+	box.writeSAMcatalog(ii, mmin=100*box.Melement)
 
+print time.time()-t0
