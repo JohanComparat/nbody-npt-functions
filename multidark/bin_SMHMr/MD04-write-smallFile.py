66d96047dd9b8f6acf4e570748a70542fdc2d6a1
JohanComparat <johan.comparat@gmail.com>
2017-04-19 23:34:26 +0200

    message

--- multidark/bin_SMHMr/MD04-write-smallFile.py
+++ multidark/bin_SMHMr/MD04-write-smallFile.py
@@ -8,7 +8,6 @@ glob
 snList = n.array(glob.glob(os.path.join(os.environ["MD04"], "snapshots", "out_*.list")))
 print snList
 box = MultiDarkSimulation(Lbox=400.0 * uu.Mpc, boxDir = "MD_0.4Gpc", snl =snList   ,zsl = None, zArray = n.arange(0.2,2.4,1e-1), Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
-box.columnDict = box.columnDictHlist
 
 t0=time.time()
 
