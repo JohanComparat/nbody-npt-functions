ae778ca0ca32f848bf0e2abc076519e39bab046e
JohanComparat <johan.comparat@gmail.com>
2017-04-20 09:15:53 +0200

    message

--- multidark/bin_SMHMr/plot-Ms-xray.py
+++ multidark/bin_SMHMr/plot-Ms-xray.py
@@ -26,13 +26,19 @@ def create_plots(env='MD04'):
 	for fileN in fileList:
 		print fileN
 		hd = fits.open(fileN)[1].data	
-		p.plot(hd['Mgal_mvir_Mo13'], hd['lambda_sar_Bo16']+hd['Mgal_mvir_Mo13'], 'b+', rasterized=True)
+		p.figure(1, (6,6))
+		p.plot(hd['Mgal_mvir_Mo13'], hd['lambda_sar_Bo16']+hd['Mgal_mvir_Mo13'], 'b,', rasterized=True)
 		p.xlabel(r'$\log_{10} M_\odot$')
 		p.ylabel(r'$\log_{10}\lambda_{SAR} + \log_{10}M_\odot$')
+		p.grid()
 		p.savefig(os.path.join(os.environ[env], "results", os.path.basename(fileN)[:-5]+'.pdf'))
-
+        p.clf()
+        
 create_plots(env='MD04')
 create_plots(env='MD10')
 create_plots(env='MD25')
 
+os.system("cp $MD04/results/*.pdf ~/wwwDir/eRoMok/plots/MD_0.4Gpc")
+os.system("cp $MD10/results/*.pdf ~/wwwDir/eRoMok/plots/MD_1.0Gpc")
+os.system("cp $MD25/results/*.pdf ~/wwwDir/eRoMok/plots/MD_2.5Gpc")
 
