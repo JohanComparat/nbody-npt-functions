f8fa1966ade894c155d88e287b3028e469460f1f
JohanComparat <johan.comparat@gmail.com>
2017-04-24 20:46:49 +0200

    message

--- multidark/bin_SMHMr/plot_Lxcut_Mhalo.py
+++ multidark/bin_SMHMr/plot_Lxcut_Mhalo.py
@@ -20,6 +20,58 @@ print " set up box, and redshift "
 #MD 1 hlist_0.74980_SAM_Nb_0.fits
 #MD 25 hlist_0.75440_SAM_Nb_10.fits
 
+
+
+bins = n.arange(10,15,0.1)
+xb = (bins[1:] + bins[:-1]) / 2.
+
+def create_plots(env='MD04'):
+	fileList = n.array(glob.glob(os.path.join(os.environ[env], "catalogs", "*.Ms.fits")))
+	print fileList
+	Hall = n.zeros((len(fileList), len(bins)-1))
+	H420 = n.zeros((len(fileList), len(bins)-1))
+	H425 = n.zeros((len(fileList), len(bins)-1))
+	H430 = n.zeros((len(fileList), len(bins)-1))
+	H435 = n.zeros((len(fileList), len(bins)-1))
+	for ii, fileN in enumerate(fileList):
+		print fileN
+		hd = fits.open(fileN)[1].data	
+		cut420 = (hd['lambda_sar_Bo16']+hd['Mgal_mvir_Mo13'] > 42.)
+		cut425 = (hd['lambda_sar_Bo16']+hd['Mgal_mvir_Mo13'] > 42.5)
+		cut430 = (hd['lambda_sar_Bo16']+hd['Mgal_mvir_Mo13'] > 43.)
+		cut435 = (hd['lambda_sar_Bo16']+hd['Mgal_mvir_Mo13'] > 43.5)
+		Hall[ii],bb = n.histogram(hd['mvir'], bins=bins)
+		H420[ii],bb = n.histogram(hd['mvir'][cut420], bins=bins)
+		H425[ii],bb = n.histogram(hd['mvir'][cut425], bins=bins)
+		H430[ii],bb = n.histogram(hd['mvir'][cut430], bins=bins)
+		H435[ii],bb = n.histogram(hd['mvir'][cut435], bins=bins)
+	
+	all_halos_i = n.sum(Hall, axis=0)
+	sel = (all_halos_i>0)
+	all_halos = all_halos_i[sel]
+	                                                              
+	p.figure(1, (6,6))
+	p.plot(xb, n.sum(H420, axis=0)[sel]/all_halos, label= 'L>42.0')
+	p.plot(xb, n.sum(H425, axis=0)[sel]/all_halos, label= 'L>42.5')
+	p.plot(xb, n.sum(H430, axis=0)[sel]/all_halos, label= 'L>43.0')
+	p.plot(xb, n.sum(H435, axis=0)[sel]/all_halos, label= 'L>43.5')
+	p.xlabel(r'$\log_{10} M_\odot$')
+	p.ylabel(r'normed cumulative distribution')
+	p.grid()
+	p.legend(frameon=False)
+	p.savefig(os.path.join(os.environ[env], "results", os.path.basename(fileN)[:-5]+'_HOD_LX.pdf'))
+	p.clf()
+	
+create_plots(env='MD04')
+create_plots(env='MD10')
+create_plots(env='MD25')
+
+os.system("cp $MD04/results/*.pdf ~/wwwDir/eRoMok/plots/MD_0.4Gpc/")
+os.system("cp $MD10/results/*.pdf ~/wwwDir/eRoMok/plots/MD_1.0Gpc/")
+os.system("cp $MD25/results/*.pdf ~/wwwDir/eRoMok/plots/MD_2.5Gpc/")
+
+sys.exit()
+
 bins = n.arange(10,15,0.1)
 
 def create_plots(env='MD04'):
