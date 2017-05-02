b997b0221e241993cebb412217d319f2453287e6
JohanComparat <johan.comparat@gmail.com>
2017-05-02 14:39:30 +0200

    message

--- multidark/bin_SMHMr/plot_slice_simulation.py
+++ multidark/bin_SMHMr/plot_slice_simulation.py
@@ -55,7 +55,7 @@ def get_slice(env='MD04', file_type="out", aexp='0.74230'):
 		return xd[zone], yd[zone], zd[zone], stellar_mass[zone], LX_AGN[zone], LX_cluster[zone], active[zone]
 
 	y, x, z, mass, LX_AGN, LX_cluster, active = get_plot_data(fileList[0])
-	
+	"""
 	for fileN in fileList[1:]:
 		print fileN
 		y_i, x_i, z_i, mass_i, LX_AGN_i, LX_cluster_i, active_i = get_plot_data(fileN)
@@ -66,21 +66,21 @@ def get_slice(env='MD04', file_type="out", aexp='0.74230'):
 		LX_AGN = n.hstack((LX_AGN, LX_AGN_i))
 		LX_cluster = n.hstack((LX_cluster, LX_cluster_i))
 		active = n.hstack((active, active_i))
-
+	"""
 	print "N points total", len(x)
 
 	def plot_slice(y, x, z, mass, LX_AGN, LX_cluster):
-		agn_selection = (active)&(LX_AGN>42)
-		cluster_selection = (LX_cluster>42)
+		LX_cut = 41.5
+		agn_selection = (active)&(LX_AGN>LX_cut)
+		cluster_selection = (LX_cluster>LX_cut)
 		print "N AGN in plot=", len(y[agn_selection])
 		print "N cluster in plot=", len(y[cluster_selection])
 		p.figure(1, (9,9))
-		p.plot(x[agn_selection], y[agn_selection], 'b+', label="AGN LX>42", rasterized=True)
-		p.plot(x[cluster_selection], y[cluster_selection], 'ro', label="Cluster LX>42", rasterized=True)
+		p.plot(x[agn_selection], y[agn_selection], 'b+', label="AGN LX>"+str(LX_cut), rasterized=True)
+		p.plot(x[cluster_selection], y[cluster_selection], 'ro', label="Cluster LX>"+str(LX_cut), rasterized=True, edgecolor='r')
 		p.xlabel(r'$x [Mpc/h]$')
 		p.ylabel(r'$y [Mpc/h]$')
 		p.grid()
-		p.yscale('log')
 		p.xlim((xmin, xmax))
 		p.ylim((ymin, ymax))
 		p.legend(frameon=False)
@@ -95,5 +95,5 @@ get_slice(env='MD04', file_type="out", aexp='0.74230')
 os.system("cp $MD04/results/*.pdf ~/wwwDir/eRoMok/plots/MD_0.4Gpc/")
 get_slice(env='MD10', file_type="out", aexp='0.74980')
 os.system("cp $MD10/results/*.pdf ~/wwwDir/eRoMok/plots/MD_1.0Gpc/")
-get_slice(env='MD25', file_type="out", aexp='0.75440')
-os.system("cp $MD25/results/*.pdf ~/wwwDir/eRoMok/plots/MD_2.5Gpc/")
+#get_slice(env='MD25', file_type="out", aexp='0.75440')
+#os.system("cp $MD25/results/*.pdf ~/wwwDir/eRoMok/plots/MD_2.5Gpc/")
