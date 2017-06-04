91a70a19b5e67e3d3af9d8c17944ccb575928a1c
JohanComparat <johan.comparat@gmail.com>
2017-06-04 21:40:24 +0200

    message

--- multidark/bin_SMHMr/MD10-write-clusterFiles.py
+++ multidark/bin_SMHMr/MD10-write-clusterFiles.py
@@ -1,17 +1,18 @@
 from MultiDark import *
 import sys
 import glob
-import os 
+import os
 
+snList= n.array(glob.glob(os.path.join(os.environ["MD10"], "snapshots", "out_*p.list")))
+snList.sort()
 
-
-snList= n.array(glob.glob(os.path.join(os.environ["MD10"], "snapshots", "out_???.list")))
 print snList
 box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc, boxDir = "MD_1Gpc",snl =snList   ,zsl = None,zArray = n.arange(0.2,2.4,1e-1),Hbox = 67.77 * uu.km / (uu.s * uu.Mpc))
 
 t0=time.time()
 
-for ii in n.arange(len(box.snl)):
-	box.writeCLUSTERcatalog(ii, mmin=6*10**12)
+for ii in n.arange(4,len(box.snl),1):
+        print ii, box.snl[ii]
+        box.writeCLUSTERcatalog(ii, mmin=6*10**12)
 
 print time.time()-t0
