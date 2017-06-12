260365e80ea5071334417325c0e1686227c6a4e2
JohanComparat <johan.comparat@gmail.com>
2017-06-12 20:20:27 +0200

    message

--- bin/bin_SMHMr/MD10-write-clusterFiles.py
+++ bin/bin_SMHMr/MD10-write-clusterFiles.py
@@ -12,9 +12,9 @@ box = MultiDarkSimulation(Lbox=1000.0 * uu.Mpc, boxDir = "MD_1Gpc",snl =snList  
 
 t0=time.time()
 
-for ii in n.arange(len(box.snl),1):
-	if not os.path.isfile(box.snl[ii][:-5]+"_cluster_Nb_0.fits") :
-		print ii, box.snl[ii]
+for ii in n.arange(len(box.snl)):
+	print ii, box.snl[ii], os.path.isfile(box.snl[ii][:-5]+"_cluster_Nb_0.fits")
+	if os.path.isfile(box.snl[ii][:-5]+"_cluster_Nb_0.fits")==False :
 		if sf['N_columns'][ii]==34 :
 			box.columnDict = {'id': 0, 'desc_id': 1, 'mvir': 2, 'vmax': 3, 'vrms': 4, 'rvir': 5, 'rs': 6, 'Np': 7, 'x': 8, 'y': 9, 'z': 10, 'vx': 11, 'vy': 12, 'vz': 13, 'Jx': 14, 'Jy': 15, 'Jz': 16, 'Spin':17, 'Rs_Klypin': 18, 'Mmvir_all': 19, 'M200b': 20, 'M200c': 21, 'M500c': 22, 'M2500c': 23, 'Xoff': 24, 'Voff': 25, 'Spin_Bullock': 26, 'b_to_a': 27, 'c_to_a': 28, 'Ax': 29, 'Ay': 30, 'Az': 31, 'pid': 32}
 		if sf['N_columns'][ii]==41 :
