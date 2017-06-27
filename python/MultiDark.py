4ccdded1f740f8477466a66ecd7fa31c62baaeea
JohanComparat <johan.comparat@gmail.com>
2017-06-27 21:15:45 +0200

    message

--- python/MultiDark.py
+++ python/MultiDark.py
@@ -70,7 +70,7 @@ class MultiDarkSimulation :
 			self.Melement = 9.63 * 10**7 # Msun
 			#self.vmin = 4* (self.Melement*self.Msun*self.G/(self.force_resolution*u.kpc.to('cm')))**0.5 * u.cm.to('km')
 
-		if self.boxDir == "MD_1Gpc":
+		if self.boxDir == "MD_1Gpc" or self.boxDir == "MD_1.0Gpc":
 			self.Melement = 1.51 * 10**9. # Msun
 			#self.vmin = 4* (self.Melement*self.Msun*self.G/(self.force_resolution*u.kpc.to('cm')))**0.5 * u.cm.to('km')
 
@@ -83,7 +83,7 @@ class MultiDarkSimulation :
 			self.vmin = 4* (self.Melement*self.Msun*self.G/(self.force_resolution*u.kpc.to('cm')))**0.5 * u.cm.to('km')
 			self.columnDict = {'id': 0, 'desc_id': 1, 'mvir': 2, 'vmax': 3, 'vrms': 4, 'rvir': 5, 'rs': 6, 'Np': 7, 'x': 8, 'y': 9, 'z': 10, 'vx': 11, 'vy': 12, 'vz': 13, 'Jx': 14, 'Jy': 15, 'Jz': 16, 'Spin':17, 'Rs_Klypin': 18, 'Mmvir_all': 19, 'M200b': 20, 'M200c': 21, 'M500c': 22, 'M2500c': 23, 'Xoff': 24, 'Voff': 25, 'Spin_Bullock': 26, 'b_to_a': 27, 'c_to_a': 28, 'Ax': 29, 'Ay': 30, 'Az': 31, 'b_to_a_500c': 32, 'c_to_a_500c': 33, 'Ax_500c': 34, 'Ay_500c': 35, 'Az_500c': 36, 'TU': 37, 'M_pe_Behroozi': 38, 'M_pe_Diemer': 39, 'Halfmass_Radius': 40, 'pid': 41}
 
-		if self.boxDir == "MD_4Gpc":
+		if self.boxDir == "MD_4Gpc" or self.boxDir == "MD_4.0Gpc":
 			self.Melement = 9.6 * 10**10. # Msun
 			self.Npart = 4096
 			#self.vmin = 4* (self.Melement*self.Msun*self.G/(self.force_resolution*u.kpc.to('cm')))**0.5 * u.cm.to('km')
