11b804efd2deb98c43f3577e5a236103b9c7891b
JohanComparat <johan.comparat@gmail.com>
2017-06-30 17:27:04 +0200

    message

--- bin/bin_SMHMr/MD10_add_4MOST_AGN.py
+++ bin/bin_SMHMr/MD10_add_4MOST_AGN.py
@@ -23,54 +23,47 @@ z_snap = summ['redshift']
 
 N_agn_per_snap = (volume * dN_dV_agn(z_snap) ).astype('int')
 
-# determine dz
-#z_middle = (summ['redshift'][1:]+summ['redshift'][:-1])*0.5
-#z_mins = n.hstack((summ['redshift'][0], z_middle))
-#z_maxs = n.hstack((z_middle, summ['redshift'][-1]))
-#dz = z_maxs - z_mins
-
 for ii, el in enumerate(summ):
 	print(N_agn_per_snap[ii])
 	N_agn = N_agn_per_snap[ii]
 
+	fileList_snap_MS = n.array(glob.glob(os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_?_Ms.fits')))
+	fileList_snap_MS.sort()
 	fileList_snap_X = n.array(glob.glob(os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_?_Xray.fits')))
 	fileList_snap_X.sort()
 
 	if N_agn > 10 :
-		MH = n.array([fits.open(file)[1].data['mvir'] for file in fileList_snap_X])
-		# ELG select on Mvir
-		#p_elg=[10**(12.2),0.25]
-		mh_mean, mh_scatter = 12.2, 0.25
-		mh_bins = n.arange(mh_mean -2*mh_scatter, mh_mean +2*mh_scatter+0.1, 0.1)
-		mh_bins_pos = 0.5*(mh_bins[1:]+mh_bins[:-1])
-		proba = lambda x : norm.pdf(x, loc=12.2,scale=0.25)
-		proba_norm = proba(mh_bins_pos).sum()
-		N_2_select_per_bin = (N_agn*proba(mh_bins_pos)/proba_norm).astype('int')
+		# LRG: select on Ms
+		MS = n.array([fits.open(file)[1].data['stellar_mass_Mo13_mvir'] for file in fileList_snap_MS])
+		SAR = n.array([fits.open(file)[1].data['lambda_sar_Bo16'] for file in fileList_snap_X])
+		act = n.array([fits.open(file)[1].data['activity'] for file in fileList_snap_X])
 
-		id_0 = []
-		id_1 = []
-		for id_bin in range(len(mh_bins)-1):
-			id_superset = n.where( (MH > mh_bins[id_bin]) &( MH < mh_bins[id_bin+1]) )
-			N_avail = id_superset[0].shape[0]
-			rds = n.random.rand(len(id_superset[0]))
-			bin_selection = (rds < N_2_select_per_bin[id_bin]*1./N_avail)
-			id_0.append(id_superset[0][bin_selection])
-			id_1.append(id_superset[1][bin_selection])
+		LX = MS + SAR
 
-		id_0 = n.hstack((id_0))
-		id_1 = n.hstack((id_1))
+		all_LX = n.hstack((LX[act]))
+		all_LX_sort_id = n.argsort(all_LX)
+		min_LX = all_LX[all_LX_sort_id[-N_agn*2-1]]
 
-		for num in list(set(id_0)):
-			file_out = os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_'+str(num)+'_4MOST_S8_ELG.fits')
-			elgs = id_1[ (id_0==num) ]
-			print( file_out, elgs)
+		id_superset = n.where(LX>min_LX)
+		active = act[id_superset]
+		id_superset_0 = id_superset[0][active]
+		id_superset_1 = id_superset[1][active]
+
+		rds = n.random.rand(len(id_superset_0))
+		# factor of 2 incompleteness due to obscured AGN 
+		# fraction of obsecured = 0.5 is random as a functino of luminiosity
+
+		for num in list(set(id_superset_0)):
+			file_out = os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_'+str(num)+'_4MOST_S6_AGN.fits')
+			agns = id_superset_1[ (id_superset_0==num) & (rds>0.5) ]
+			print( file_out, agns, len(agns))
 			hdu_cols  = fits.ColDefs([
-			fits.Column(name='line_number',format='K', array= elgs )])
+			fits.Column(name='line_number',format='K', array= agns )])
 			tb_hdu = fits.BinTableHDU.from_columns( hdu_cols )
 			prihdr = fits.Header()
 			prihdr['HIERARCH nameSnapshot'] = el['snap_name']
 			prihdr['batchN'] = num
-			prihdr['tracer'] = 'ELG'
+			prihdr['tracer'] = 'AGN'
 			prihdr['author'] = 'JC'
 			prihdu = fits.PrimaryHDU(header=prihdr)
 			#writes the file
@@ -78,5 +71,7 @@ for ii, el in enumerate(summ):
 			if os.path.isfile(file_out):
 				os.system("rm "+file_out)
 			thdulist.writeto(file_out)
+
+
 
 
