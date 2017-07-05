4a55bf98d9a631ce2d66ccfc3e78ff9ef23e8dc5
JohanComparat <johan.comparat@gmail.com>
2017-07-05 14:27:47 +0200

    message

--- bin/bin_SMHMr/MD10-pie-plot.py
+++ bin/bin_SMHMr/MD10-pie-plot.py
@@ -9,7 +9,7 @@ import glob
 import cPickle
 
 import matplotlib
-#matplotlib.use('Agg')
+matplotlib.use('Agg')
 import matplotlib.pyplot as p
 
 from scipy.interpolate import interp1d
@@ -17,8 +17,8 @@ from scipy.interpolate import interp1d
 L_box = 1000./0.6777
 
 tracer_names = n.array(['S8_ELG', 'S8_LRG', 'S5_GAL', 'S8_QSO', 'S6_AGN', 'S5_BCG'])
-marker_dict={'S5_BCG':'1', 'S5_GAL':'2', 'S6_AGN':'3', 'S8_LRG':'4', 'S8_ELG':'+', 'S8_QSO':'x'}
-color_dict ={'S5_BCG':'r', 'S5_GAL':'r', 'S6_AGN':'c', 'S8_LRG':'k', 'S8_ELG':'b', 'S8_QSO':'g'}
+marker_dict={'S5_BCG':'1', 'S5_GAL':'2', 'S6_AGN':'3', 'S8_LRG':',', 'S8_ELG':',', 'S8_QSO':'x'}
+color_dict ={'S5_BCG':'r', 'S5_GAL':'r', 'S6_AGN':'m', 'S8_LRG':'k', 'S8_ELG':'b', 'S8_QSO':'g'}
 p0 = n.array([[-1., -1.]])
 points = {'S5_BCG':p0, 'S5_GAL':p0, 'S6_AGN':p0, 'S8_LRG':p0, 'S8_ELG':p0, 'S8_QSO':p0}
 
@@ -63,18 +63,18 @@ def get_slice(cpickle_dump_file, x_observer=0., y_observer=0., z_observer = 0., 
 			#path_2_tracer_file = tracer_files[0]
 			for path_2_tracer_file in tracer_files:
 				print path_2_tracer_file
-			spl_bn = os.path.basename(path_2_tracer_file)[:-5].split('_')
-			tracer_name = spl_bn[-2]+'_'+spl_bn[-1]
-			ids = fits.open(path_2_tracer_file)[1].data['line_number']
+				spl_bn = os.path.basename(path_2_tracer_file)[:-5].split('_')
+				tracer_name = spl_bn[-2]+'_'+spl_bn[-1]
+				ids = fits.open(path_2_tracer_file)[1].data['line_number']
 
-			x_i = positions['x'][ids]/0.6777 - x_observer + x_shift
-			y_i = positions['y'][ids]/0.6777 - y_observer + y_shift
-			z_i = positions['z'][ids]/0.6777 - z_observer + z_shift
+				x_i = positions['x'][ids]/0.6777 - x_observer + x_shift
+				y_i = positions['y'][ids]/0.6777 - y_observer + y_shift
+				z_i = positions['z'][ids]/0.6777 - z_observer + z_shift
 
-			shell = (x_i*x_i + y_i*y_i + z_i*z_i < r_max**2.) & (x_i*x_i + y_i*y_i + z_i*z_i > r_min**2.)
-			slice = (shell) & (z_i>slice_z_min) &(z_i<slice_z_max)
+				shell = (x_i*x_i + y_i*y_i + z_i*z_i < r_max**2.) & (x_i*x_i + y_i*y_i + z_i*z_i > r_min**2.)
+				slice = (shell) & (z_i>slice_z_min) &(z_i<slice_z_max)
 
-			points[tracer_name] = n.vstack(( points[tracer_name], n.transpose([x_i[slice], y_i[slice]]) ))
+				points[tracer_name] = n.vstack(( points[tracer_name], n.transpose([x_i[slice], y_i[slice]]) ))
 
 
 	f=open(cpickle_dump_file, 'w')
@@ -83,34 +83,47 @@ def get_slice(cpickle_dump_file, x_observer=0., y_observer=0., z_observer = 0., 
 	return points
 
 
-points_1 = get_slice(os.path.join(work_dir, 'slice_1_Lbox.pkl'))
-points_2 = get_slice(os.path.join(work_dir, 'slice_2_Lbox.pkl'), x_shift = L_box, distance_min=L_box, distance_max = L_box*2.)
+#points_1 = get_slice(os.path.join(work_dir, 'slice_1_Lbox.pkl'))
+#points_2 = get_slice(os.path.join(work_dir, 'slice_2_Lbox.pkl'), x_shift = L_box, distance_min=L_box, distance_max = 2*L_box)
+points_3 = get_slice(os.path.join(work_dir, 'slice_3_Lbox.pkl'), x_shift = 2*L_box, distance_min=2*L_box, distance_max = 3*L_box)
+points_4 = get_slice(os.path.join(work_dir, 'slice_4_Lbox.pkl'), x_shift = 3*L_box, distance_min=3*L_box, distance_max = 4*L_box)
 
-p.figure(0, (4.5,4.5))
-p.axes([0.17,0.17,0.78,0.78])
-for tracer in tracer_names:
-	x_pos, y_pos = points_1[tracer].T
-	p.plot(x_pos, y_pos,marker=marker_dict[tracer],color=color_dict[tracer],rasterized=True,ls='None',label=tracer)
+points_1 = cPickle.load(open(os.path.join(work_dir, 'slice_1_Lbox.pkl'),'r'))
+points_2 = cPickle.load(open(os.path.join(work_dir, 'slice_2_Lbox.pkl'),'r'))
+points_3 = cPickle.load(open(os.path.join(work_dir, 'slice_3_Lbox.pkl'),'r'))
+points_4 = cPickle.load(open(os.path.join(work_dir, 'slice_4_Lbox.pkl'),'r'))
 
-p.legend(loc=0, frameon=False)
-p.xlabel('Mpc')
-p.ylabel('Mpc')
-p.xlim((0,L_box))
-p.ylim((0,L_box))
-p.savefig(os.path.join(plot_dir, 'slice_1_Lbox.png'))
-p.clf()
+def plot_slice(points, name='slice_1_Lbox.png', lims=(0,L_box)) :
+	p.figure(0, ((6,6)))
+	p.axes([0.17,0.17,0.78,0.78])
+	for tracer in tracer_names:
+		x_pos, y_pos = points[tracer].T
+		p.plot(x_pos, y_pos,marker=marker_dict[tracer],color=color_dict[tracer],rasterized=True,ls='None',label=tracer)
 
+	p.legend(loc=0, frameon=False, fontsize=9)
+	p.xlabel('Mpc')
+	p.ylabel('Mpc')
+	p.xlim(lims)
+	p.ylim((0,L_box))
+	p.title(str(n.round(dc_2_z(lims[0]),2))+'<z<'+str(n.round(dc_2_z(lims[1]),2)) )
+	p.savefig(os.path.join(plot_dir, name))
+	p.clf()
 
-p.figure(0, (4.5,4.5))
+plot_slice(points_1, name='slice_1_Lbox.png', lims=(0*L_box,1*L_box))
+plot_slice(points_2, name='slice_2_Lbox.png', lims=(1*L_box,2*L_box))
+plot_slice(points_3, name='slice_3_Lbox.png', lims=(2*L_box,3*L_box))
+plot_slice(points_4, name='slice_4_Lbox.png', lims=(3*L_box,4*L_box))
+
+p.figure(0, ((6,6)))
 p.axes([0.17,0.17,0.78,0.78])
 for tracer in tracer_names:
 	x_pos, y_pos = points_2[tracer].T
 	p.plot(x_pos, y_pos,marker=marker_dict[tracer],color=color_dict[tracer],rasterized=True,ls='None',label=tracer)
 
-p.legend(loc=0, frameon=False)
+p.legend(loc=0, frameon=False, fontsize=9)
 p.xlabel('Mpc')
 p.ylabel('Mpc')
-p.xlim((L_box,2*L_box))
+p.xlim(lims)
 p.ylim((0.,L_box))
 p.savefig(os.path.join(plot_dir, 'slice_2_Lbox.png'))
 p.clf()
