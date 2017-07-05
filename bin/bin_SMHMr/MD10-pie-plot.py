35f25fc94fe188fcb404ae990db4ea5fdb48ac7b
JohanComparat <johan.comparat@gmail.com>
2017-07-05 14:27:42 +0200

    message

--- bin/bin_SMHMr/MD10-pie-plot.py
+++ bin/bin_SMHMr/MD10-pie-plot.py
@@ -38,57 +38,53 @@ work_dir = os.path.join(os.environ['MD10'],'work_agn')
 
 # redshift loop
 #ii = 0
-def get_slice(cpickle_dump_file, x_observer=0., y_observer=0., z_observer = 0., x_shift=0., y_shift=0., z_shift=0., slice_z_min=0., slice_z_max = 10., distance_min=0., distance_max = L_box):
-    
-    snap_selection = (sf['comoving_distance']<distance_max)&(sf['comoving_distance']>distance_min)
+def get_slice(cpickle_dump_file, x_observer=0., y_observer=0., z_observer = 0., x_shift=0., y_shift=0., z_shift=0., slice_z_min=0., slice_z_max = 10., distance_min=0., distance_max = L_box):    
+	snap_selection = (sf['comoving_distance']<distance_max)&(sf['comoving_distance']>distance_min)
+	snaps = sf[snap_selection]
+	z_all = sf['redshift'][snap_selection]
+	z_boundaries = n.hstack((dc_2_z(distance_min), (z_all[1:]+z_all[:-1])/2., dc_2_z(distance_max)))
+	for ii, el in enumerate(snaps): # in range(len(z_all)):
+		z_min, z_max = z_boundaries[ii], z_boundaries[ii+1]
+		r_min, r_max = cosmoMD.comoving_distance(z_min).value, cosmoMD.comoving_distance(z_max).value
 
-    z_all = sf['redshift'][snap_selection]
-    z_boundaries = n.hstack((dc_2_z(distance_min), (z_all[1:]+z_all[:-1])/2., dc_2_z(distance_max)))
+		position_files = n.array(glob.glob(os.path.join(work_dir, 'out_'+el['snap_name']+'_SAM_Nb_?.fits')))
+		position_files.sort()
 
-    for ii in range(len(z_all)):
-	z_min, z_max = z_boundaries[ii], z_boundaries[ii+1]
-	el = sf[ii+1]
+		# position file loop
+		print r_min, r_max
+		for index in range(len(position_files)):
+			print time.time()-t0
+			print position_files[index]
+			positions = fits.open(position_files[index])[1].data
+			tracer_files = n.array(glob.glob(os.path.join(work_dir, 'out_'+el['snap_name']+'_SAM_Nb_'+str(index)+'_4MOST_*.fits')))
+			tracer_files.sort()
 
-	r_min, r_max = cosmoMD.comoving_distance(z_min).value, cosmoMD.comoving_distance(z_max).value
+			# tracer loop
+			#path_2_tracer_file = tracer_files[0]
+			for path_2_tracer_file in tracer_files:
+				print path_2_tracer_file
+			spl_bn = os.path.basename(path_2_tracer_file)[:-5].split('_')
+			tracer_name = spl_bn[-2]+'_'+spl_bn[-1]
+			ids = fits.open(path_2_tracer_file)[1].data['line_number']
 
-	position_files = n.array(glob.glob(os.path.join(work_dir, 'out_'+el['snap_name']+'_SAM_Nb_?.fits')))
-	position_files.sort()
+			x_i = positions['x'][ids]/0.6777 - x_observer + x_shift
+			y_i = positions['y'][ids]/0.6777 - y_observer + y_shift
+			z_i = positions['z'][ids]/0.6777 - z_observer + z_shift
 
-	# position file loop
-	print r_min, r_max
-	for index in range(len(position_files)):
-	    print time.time()-t0
-	    print position_files[index]
-	    positions = fits.open(position_files[index])[1].data
-	    tracer_files = n.array(glob.glob(os.path.join(work_dir, 'out_'+el['snap_name']+'_SAM_Nb_'+str(index)+'_4MOST_*.fits')))
-	    tracer_files.sort()
+			shell = (x_i*x_i + y_i*y_i + z_i*z_i < r_max**2.) & (x_i*x_i + y_i*y_i + z_i*z_i > r_min**2.)
+			slice = (shell) & (z_i>slice_z_min) &(z_i<slice_z_max)
 
-	    # tracer loop
-	    #path_2_tracer_file = tracer_files[0]
-	    for path_2_tracer_file in tracer_files:
-    		print path_2_tracer_file
-		spl_bn = os.path.basename(path_2_tracer_file)[:-5].split('_')
-		tracer_name = spl_bn[-2]+'_'+spl_bn[-1]
-		ids = fits.open(path_2_tracer_file)[1].data['line_number']
-
-		x_i = positions['x'][ids]/0.6777 - x_observer + x_shift
-		y_i = positions['y'][ids]/0.6777 - y_observer + y_shift
-		z_i = positions['z'][ids]/0.6777 - z_observer + z_shift
-
-		shell = (x_i*x_i + y_i*y_i + z_i*z_i < r_max**2.) & (x_i*x_i + y_i*y_i + z_i*z_i > r_min**2.)
-		slice = (shell) & (z_i>slice_z_min) &(z_i<slice_z_max)
-
-		points[tracer_name] = n.vstack(( points[tracer_name], n.transpose([x_i[slice], y_i[slice]]) ))
+			points[tracer_name] = n.vstack(( points[tracer_name], n.transpose([x_i[slice], y_i[slice]]) ))
 
 
-    f=open(cpickle_dump_file, 'w')
-    cPickle.dump(points,f)
-    f.close()
-    return points
+	f=open(cpickle_dump_file, 'w')
+	cPickle.dump(points,f)
+	f.close()
+	return points
 
 
 points_1 = get_slice(os.path.join(work_dir, 'slice_1_Lbox.pkl'))
-points_2 = get_slice(os.path.join(work_dir, 'slice_2_Lbox.pkl'),x_shift=L_box,distance_min=L_box, distance_max = L_box*2.)
+points_2 = get_slice(os.path.join(work_dir, 'slice_2_Lbox.pkl'), x_shift = L_box, distance_min=L_box, distance_max = L_box*2.)
 
 p.figure(0, (4.5,4.5))
 p.axes([0.17,0.17,0.78,0.78])
