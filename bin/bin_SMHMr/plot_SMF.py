3bec84d1dbea1773971e9e2e91a869befa172052
JohanComparat <johan.comparat@gmail.com>
2017-07-18 08:07:29 +0200

    message

--- bin/bin_SMHMr/plot_SMF.py
+++ bin/bin_SMHMr/plot_SMF.py
@@ -71,9 +71,11 @@ def plot_SMF_DC(snap_name, redshift):
 	
 	
 	def plot_tracer(tracer_name='4MOST_S5_BCG'):
-		logMs_low, logMs_up, counts, dN_dVdlogM_g = n.loadtxt(out_file(tracer_name ) , unpack=True )
-		ok = (dN_dVdlogM_g>0)
-		p.plot((logMs_low[ok] + logMs_up[ok])/2., n.log10(dN_dVdlogM_g[ok]), label=tracer_name, ls='dotted', lw=0.75)
+		file_name = out_file(tracer_name )
+		if os.path.isfile(file_name) :
+			logMs_low, logMs_up, counts, dN_dVdlogM_g = n.loadtxt(file_name  , unpack=True )
+			ok = (dN_dVdlogM_g>0)
+			p.plot((logMs_low[ok] + logMs_up[ok])/2., n.log10(dN_dVdlogM_g[ok]), label=tracer_name, ls='dotted', lw=0.75)
 
 	plot_tracer("4MOST_S5_BCG" )
 	plot_tracer("4MOST_S5_GAL" )
