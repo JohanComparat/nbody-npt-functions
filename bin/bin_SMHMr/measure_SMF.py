ccc68318207b76d025ac2347d0b9c5ca5be64dc6
JohanComparat <johan.comparat@gmail.com>
2017-07-18 11:46:47 +0200

    message

--- bin/bin_SMHMr/measure_SMF.py
+++ bin/bin_SMHMr/measure_SMF.py
@@ -44,8 +44,8 @@ def measureSMF(snap_name, env='MD10', volume=1000.**3., out_dir="../"):
 
 
 def measureSMF_tracer(snap_name, tracer_name, env='MD10', volume=1000.**3., out_dir="../"):
-	out_file = os.path.join(out_dir, "out_"+snap_name+"_"+tracer_name+"_SMF.txt")
-	if os.path.isfile(out_file)==False:
+		#out_file = os.path.join(out_dir, "out_"+snap_name+"_"+tracer_name+"_SMF.txt")
+		#if os.path.isfile(out_file)==False:
 		fileList = n.array(glob.glob(os.path.join(os.environ[env], "work_agn", "out_"+snap_name+"_SAM_Nb_*_Ms.fits")))
 		fileList.sort()
 		fileList_T = n.array(glob.glob(os.path.join(os.environ[env], "work_agn", "out_"+snap_name+"_SAM_Nb_*_"+tracer_name+".fits")))
