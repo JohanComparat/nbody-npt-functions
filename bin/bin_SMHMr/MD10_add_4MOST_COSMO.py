b2ba197898cb550254555f794825a4746e3b00e7
JohanComparat <johan.comparat@gmail.com>
2017-07-19 09:54:43 +0200

    message

--- bin/bin_SMHMr/MD10_add_4MOST_COSMO.py
+++ bin/bin_SMHMr/MD10_add_4MOST_COSMO.py
@@ -142,7 +142,7 @@ for ii, el in enumerate(summ):
 	fileList_snap_MS = n.array(glob.glob(os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_?_Ms.fits')))
 	fileList_snap_MS.sort()
 
-	if N_lrg1 > 10 :
+	if N_lrg1 > 100 :
 		# LRG: select on Ms
 		MS = n.array([fits.open(file)[1].data['stellar_mass_Mo13_mvir'] for file in fileList_snap_MS])
 
@@ -172,7 +172,7 @@ for ii, el in enumerate(summ):
 				os.system("rm "+file_out)
 			thdulist.writeto(file_out)
 
-	if N_lrg2 > 10 :
+	if N_lrg2 > 100 :
 		# LRG: select on Ms
 		MS = n.array([fits.open(file)[1].data['stellar_mass_Mo13_mvir'] for file in fileList_snap_MS])
 
@@ -202,91 +202,91 @@ for ii, el in enumerate(summ):
 				os.system("rm "+file_out)
 			thdulist.writeto(file_out)
 
-	if N_elg > 10 or N_qso > 10  :
+	
+	if N_elg > 100 :
 		MH = n.array([fits.open(file)[1].data['mvir'] for file in fileList_snap])
+		# ELG select on Mvir
+		#p_elg=[10**(12.2),0.25]
+		mh_mean, mh_scatter = 12.2, 0.25
+		mh_bins = n.arange(mh_mean -2*mh_scatter, mh_mean +2*mh_scatter+0.1, 0.1)
+		mh_bins_pos = 0.5*(mh_bins[1:]+mh_bins[:-1])
+		proba = lambda x : norm.pdf(x, loc=mh_mean,scale=mh_scatter)
+		proba_norm = proba(mh_bins_pos).sum()
+		N_2_select_per_bin = (N_elg*proba(mh_bins_pos)/proba_norm).astype('int')
 
-		if N_elg > 10 :
-			# ELG select on Mvir
-			#p_elg=[10**(12.2),0.25]
-			mh_mean, mh_scatter = 12.2, 0.25
-			mh_bins = n.arange(mh_mean -2*mh_scatter, mh_mean +2*mh_scatter+0.1, 0.1)
-			mh_bins_pos = 0.5*(mh_bins[1:]+mh_bins[:-1])
-			proba = lambda x : norm.pdf(x, loc=mh_mean,scale=mh_scatter)
-			proba_norm = proba(mh_bins_pos).sum()
-			N_2_select_per_bin = (N_elg*proba(mh_bins_pos)/proba_norm).astype('int')
+		id_0 = []
+		id_1 = []
+		for id_bin in range(len(mh_bins)-1):
+			id_superset = n.where( (MH > mh_bins[id_bin]) &( MH < mh_bins[id_bin+1]) )
+			N_avail = id_superset[0].shape[0]
+			rds = n.random.rand(len(id_superset[0]))
+			bin_selection = (rds < N_2_select_per_bin[id_bin]*1./N_avail)
+			id_0.append(id_superset[0][bin_selection])
+			id_1.append(id_superset[1][bin_selection])
 
-			id_0 = []
-			id_1 = []
-			for id_bin in range(len(mh_bins)-1):
-				id_superset = n.where( (MH > mh_bins[id_bin]) &( MH < mh_bins[id_bin+1]) )
-				N_avail = id_superset[0].shape[0]
-				rds = n.random.rand(len(id_superset[0]))
-				bin_selection = (rds < N_2_select_per_bin[id_bin]*1./N_avail)
-				id_0.append(id_superset[0][bin_selection])
-				id_1.append(id_superset[1][bin_selection])
+		id_0 = n.hstack((id_0))
+		id_1 = n.hstack((id_1))
 
-			id_0 = n.hstack((id_0))
-			id_1 = n.hstack((id_1))
+		for num in list(set(id_0)):
+			file_out = os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_'+str(num)+'_4MOST_S8_ELG.fits')
+			elgs = id_1[ (id_0==num) ]
+			print( file_out, elgs, len(elgs))
+			hdu_cols  = fits.ColDefs([
+			fits.Column(name='line_number',format='K', array= elgs )])
+			tb_hdu = fits.BinTableHDU.from_columns( hdu_cols )
+			prihdr = fits.Header()
+			prihdr['HIERARCH nameSnapshot'] = el['snap_name']
+			prihdr['batchN'] = num
+			prihdr['tracer'] = 'ELG'
+			prihdr['author'] = 'JC'
+			prihdu = fits.PrimaryHDU(header=prihdr)
+			#writes the file
+			thdulist = fits.HDUList([prihdu, tb_hdu])
+			if os.path.isfile(file_out):
+				os.system("rm "+file_out)
+			thdulist.writeto(file_out)
 
-			for num in list(set(id_0)):
-				file_out = os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_'+str(num)+'_4MOST_S8_ELG.fits')
-				elgs = id_1[ (id_0==num) ]
-				print( file_out, elgs, len(elgs))
-				hdu_cols  = fits.ColDefs([
-				fits.Column(name='line_number',format='K', array= elgs )])
-				tb_hdu = fits.BinTableHDU.from_columns( hdu_cols )
-				prihdr = fits.Header()
-				prihdr['HIERARCH nameSnapshot'] = el['snap_name']
-				prihdr['batchN'] = num
-				prihdr['tracer'] = 'ELG'
-				prihdr['author'] = 'JC'
-				prihdu = fits.PrimaryHDU(header=prihdr)
-				#writes the file
-				thdulist = fits.HDUList([prihdu, tb_hdu])
-				if os.path.isfile(file_out):
-					os.system("rm "+file_out)
-				thdulist.writeto(file_out)
+	if N_qso > 100 :
+		MH = n.array([fits.open(file)[1].data['mvir'] for file in fileList_snap])
+		# QSO select on Mvir
+		#p_qso=[10**(12.7),0.25]
+		mh_mean, mh_scatter = 12.7, 0.25
+		mh_bins = n.arange(mh_mean -2*mh_scatter, mh_mean +2*mh_scatter+0.1, 0.1)
+		mh_bins_pos = 0.5*(mh_bins[1:]+mh_bins[:-1])
+		proba_qso = lambda x : norm.pdf(x, loc=mh_mean,scale=mh_scatter)
+		proba_norm_qso = proba_qso(mh_bins_pos).sum()
+		N_2_select_per_bin = (N_qso*proba_qso(mh_bins_pos)/proba_norm_qso).astype('int')
 
-		if N_qso > 10 :
-			# QSO select on Mvir
-			#p_qso=[10**(12.7),0.25]
-			mh_mean, mh_scatter = 12.7, 0.25
-			mh_bins = n.arange(mh_mean -2*mh_scatter, mh_mean +2*mh_scatter+0.1, 0.1)
-			mh_bins_pos = 0.5*(mh_bins[1:]+mh_bins[:-1])
-			proba = lambda x : norm.pdf(x, loc=mh_mean,scale=mh_scatter)
-			proba_norm = proba(mh_bins_pos).sum()
-			N_2_select_per_bin = (N_qso*proba(mh_bins_pos)/proba_norm).astype('int')
+		id_0 = []
+		id_1 = []
+		for id_bin in range(len(mh_bins)-1):
+			id_superset = n.where( (MH > mh_bins[id_bin]) &( MH < mh_bins[id_bin+1]) )
+			N_avail = id_superset[0].shape[0]
+			rds = n.random.rand(len(id_superset[0]))
+			bin_selection = (rds < N_2_select_per_bin[id_bin]*1./N_avail)
+			id_0.append(id_superset[0][bin_selection])
+			id_1.append(id_superset[1][bin_selection])
 
-			id_0 = []
-			id_1 = []
-			for id_bin in range(len(mh_bins)-1):
-				id_superset = n.where( (MH > mh_bins[id_bin]) &( MH < mh_bins[id_bin+1]) )
-				N_avail = id_superset[0].shape[0]
-				rds = n.random.rand(len(id_superset[0]))
-				bin_selection = (rds < N_2_select_per_bin[id_bin]*1./N_avail)
-				id_0.append(id_superset[0][bin_selection])
-				id_1.append(id_superset[1][bin_selection])
+		id_0 = n.hstack((id_0))
+		id_1 = n.hstack((id_1))
 
-			id_0 = n.hstack((id_0))
-			id_1 = n.hstack((id_1))
-
-			for num in list(set(id_0)):
-				file_out = os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_'+str(num)+'_4MOST_S8_QSO.fits')
-				qsos = id_1[ (id_0==num) ]
-				print( file_out, qsos, len(qsos))
-				hdu_cols  = fits.ColDefs([
-				fits.Column(name='line_number',format='K', array= qsos )])
-				tb_hdu = fits.BinTableHDU.from_columns( hdu_cols )
-				prihdr = fits.Header()
-				prihdr['HIERARCH nameSnapshot'] = el['snap_name']
-				prihdr['batchN'] = num
-				prihdr['tracer'] = 'QSO'
-				prihdr['author'] = 'JC'
-				prihdu = fits.PrimaryHDU(header=prihdr)
-				#writes the file
-				thdulist = fits.HDUList([prihdu, tb_hdu])
-				if os.path.isfile(file_out):
-					os.system("rm "+file_out)
-				thdulist.writeto(file_out)
+		for num in list(set(id_0)):
+			file_out = os.path.join(os.environ["MD10"], 'work_agn', 'out_'+el['snap_name']+'_SAM_Nb_'+str(num)+'_4MOST_S8_QSO.fits')
+			qsos = id_1[ (id_0==num) ]
+			print( file_out, qsos, len(qsos))
+			hdu_cols  = fits.ColDefs([
+			fits.Column(name='line_number',format='K', array= qsos )])
+			tb_hdu = fits.BinTableHDU.from_columns( hdu_cols )
+			prihdr = fits.Header()
+			prihdr['HIERARCH nameSnapshot'] = el['snap_name']
+			prihdr['batchN'] = num
+			prihdr['tracer'] = 'QSO'
+			prihdr['author'] = 'JC'
+			prihdu = fits.PrimaryHDU(header=prihdr)
+			#writes the file
+			thdulist = fits.HDUList([prihdu, tb_hdu])
+			if os.path.isfile(file_out):
+				os.system("rm "+file_out)
+			thdulist.writeto(file_out)
 
 
