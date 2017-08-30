5d7982af9378754dd8a664a36efe1a27cf06bcb0
JohanComparat <johan.comparat@gmail.com>
2017-08-30 11:37:32 +0200

    sanity plots

--- bin/bin_SMHMr/MD10-check-small-file-1pt-fun-plots.py
+++ bin/bin_SMHMr/MD10-check-small-file-1pt-fun-plots.py
@@ -15,6 +15,11 @@ from astropy.cosmology import FlatLambdaCDM
 import astropy.units as u
 
 import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as p
+
+plotDir = os.path.join(os.environ['HOME'], 'wwwDir', "eRoMok", "snapshot_sanity_checks")
 
 sf = fits.open(os.path.join(os.environ["MD10"], "output_MD_1.0Gpc.fits"))[1].data
 
@@ -23,14 +28,16 @@ for name in sf['snap_name']:
 	print path_2_file
 	if os.path.isfile(path_2_file):
 		hdus = fits.open(path_2_file)
-		ok = (hdus[1].data['mvir'] > 0)
-
 		for qty_name in hdus[1].columns.names[:-3] :
-			t0 = time.time()
-			array_qty = hdus[1].data[qty_name][ok]
-			print qty_name, n.min(array_qty), n.max(array_qty)
-			out = n.histogram(array_qty, bins = 1000)
-			f=open(os.path.join(os.environ["MD10"], 'work_agn', 'sanity_check', os.path.basename(path_2_file)[:-5] + "_" + qty_name + ".pkl"),'w')
-			cPickle.dump(out, f)
+			f=open(os.path.join(os.environ["MD10"], 'work_agn', 'sanity_check', os.path.basename(path_2_file)[:-5] + "_" + qty_name + ".pkl"),'r')
+			out = cPickle.load(f)
 			f.close()
-			print "time elapsed", time.time() - t0
+			
+			p.figure(1, (6,6))
+			p.plot((out[1][:-1]+out[1][1:])/2. , n.array([n.sum(out[0][ii:]) for ii in range(len(out[0])) ])[::-1] )
+			p.xlabel(qty_name)
+			p.ylabel('counts')
+			p.grid()
+			p.savefig(os.path.join(plotDir, os.path.basename(path_2_file)[:-5] + "_" + qty_name + ".jpg"))
+			p.clf()
+			
