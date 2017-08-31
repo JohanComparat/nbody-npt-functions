244956d43ce3c9d9405decb807af02fcf7e265f6
JohanComparat <johan.comparat@gmail.com>
2017-08-31 11:14:13 +0200

    rewrite_snapshot

--- bin/bin_SMHMr/MD10-write-smallFile.py
+++ bin/bin_SMHMr/MD10-write-smallFile.py
@@ -14,14 +14,17 @@ box = MultiDarkSimulation(Lbox=1000.0 * u.Mpc, boxDir = "MD_1Gpc",snl =snList   
 t0=time.time()
 
 #sf['snap_name'][ii]
+
+name = os.path.join(os.environ["MD10"], "snapshots", "out_"+sf['snap_name'][ii]+".list")
+
 #for ii in n.arange(len(box.snl)):
 print sf[ii]
 if sf['N_columns'][ii]==34 :
 	box.columnDict = {'id': 0, 'desc_id': 1, 'mvir': 2, 'vmax': 3, 'vrms': 4, 'rvir': 5, 'rs': 6, 'Np': 7, 'x': 8, 'y': 9, 'z': 10, 'vx': 11, 'vy': 12, 'vz': 13, 'Jx': 14, 'Jy': 15, 'Jz': 16, 'Spin':17, 'Rs_Klypin': 18, 'Mmvir_all': 19, 'M200b': 20, 'M200c': 21, 'M500c': 22, 'M2500c': 23, 'Xoff': 24, 'Voff': 25, 'Spin_Bullock': 26, 'b_to_a': 27, 'c_to_a': 28, 'Ax': 29, 'Ay': 30, 'Az': 31, 'b_to_a_500c': 32, 'pid': 33}
-	box.writeSAMcatalog(sf['snap_name'][ii], mmin=100*box.Melement)
+	box.writeSAMcatalog(name, mmin=100*box.Melement)
 if sf['N_columns'][ii]==41 :
 	box.columnDict = {'id': 0, 'desc_id': 1, 'mvir': 2, 'vmax': 3, 'vrms': 4, 'rvir': 5, 'rs': 6, 'Np': 7, 'x': 8, 'y': 9, 'z': 10, 'vx': 11, 'vy': 12, 'vz': 13, 'Jx': 14, 'Jy': 15, 'Jz': 16, 'Spin':17, 'Rs_Klypin': 18, 'Mmvir_all': 19, 'M200b': 20, 'M200c': 21, 'M500c': 22, 'M2500c': 23, 'Xoff': 24, 'Voff': 25, 'Spin_Bullock': 26, 'b_to_a': 27, 'c_to_a': 28, 'Ax': 29, 'Ay': 30, 'Az': 31, 'b_to_a_500c': 32, 'c_to_a_500c': 33, 'Ax_500c': 34, 'Ay_500c': 35, 'Az_500c': 36, 'TU': 37, 'M_pe_Behroozi': 38, 'M_pe_Diemer': 39, 'pid': 40}
-	box.writeSAMcatalog(sf['snap_name'][ii], mmin=100*box.Melement)
+	box.writeSAMcatalog(name, mmin=100*box.Melement)
 
 print "time used", time.time()-t0, "seconds"
 
